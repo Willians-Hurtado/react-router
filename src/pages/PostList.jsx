782e57bd1a13b9099aded59743a00b940f68a42d
Willians-Hurtado <willianshurtado5@gmail.com>
2025-04-01 16:42:47 +0200

    Add posts to Post List page using API

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,9 +1,35 @@
-export default function Postlist() {
+import React, { useState, useEffect } from 'react';
+
+export default function PostList() {
+    const [postList, setPostList] = useState([]);
+
+    useEffect(() => {
+        fetch("https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts")
+            .then(response => response.json())
+            .then(data => setPostList(data))
+    }, []);
 
     return (
-        <>
-            <h1>Post List</h1>
-            <p>Welcome to the Post List page!</p>
-        </>
+        <div className="container">
+            <h1 className='text-center'>Post List</h1>
+            <div className="row py-5">
+                {postList.map((post) => (
+                    <div key={post.id} className="col-md-4 mb-4">
+                        <div className="card mb-3 h-100">
+                            <div className="card-body">
+                                <h3 className="card-title">{post.title}</h3>
+                                <p className="card-text">{post.author}</p>
+                                <p className="card-text">{post.body}</p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                ))}
+            </div>
+        </div>
+
+
+
     );
 }
